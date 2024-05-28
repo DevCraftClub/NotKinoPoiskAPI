aef4b2eb06786b0941bad719a9677681fbec2934
Maxim Harder <info@maxim-harder.de>
2024-05-28 14:37:39 +0200

    build data

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup
 
 setup(
-		name="notkionpoiskapi",
-		version="1.0.0",
-		description="Неофициальная библиотека для Kinopoisk API Unofficial",
-		long_description=open("README.md", "r").read(),
-		long_description_content_type="text/markdown",
-		keywords="api kinopoisk python",
-		url="https://github.com/DevCraftClub/NotKinoPoiskAPI",
-		author="Maxim Harder",
-		author_email="dev@devcraft.club",
-		packages=["notkionpoiskapi"],
-		install_requires=[
-				"requests",
-				"python-decouple",
-		],
-		classifiers=[
-				"Programming Language :: Python :: 3.8",
-				"Programming Language :: Python :: 3.9",
-				"Programming Language :: Python :: 3.10",
-				"Programming Language :: Python :: 3.11",
-		],
-		zip_safe=False,
+	name="notkionpoiskapi",
+	version="1.0.0",
+	description="Неофициальная библиотека для Kinopoisk API Unofficial",
+	long_description=open("readme.md", "r", encoding="utf8").read(),
+	long_description_content_type="text/markdown",
+	keywords="api kinopoisk python",
+	url="https://github.com/DevCraftClub/NotKinoPoiskAPI",
+	author="Maxim Harder",
+	author_email="dev@devcraft.club",
+	license=open("LICENSE", "r", encoding="utf8").read(),
+	install_requires=[
+		"requests",
+		"python-decouple",
+	],
+	classifiers=[
+		"License :: OSI Approved :: GNUv3 License"
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
+	],
+	zip_safe=False,
 )
